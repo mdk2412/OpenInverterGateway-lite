9aa6b23f1eb7429cb372eab025a9ab49330972a3
NeuDLi <23579032+mdk2412@users.noreply.github.com>
2025-08-03 14:22:18 +0200

    Change config values

--- SRC/ShineWiFi-ModBus/Config.h
+++ SRC/ShineWiFi-ModBus/Config.h
@@ -44,7 +44,7 @@
 // If there are other inverters in the system only load the surplus via AC
 // charging
 #define ACCHARGE_CONTROL 1
-#define ACCHARGE_CONTROL_TIMER 2000     // 1s default
+#define ACCHARGE_CONTROL_TIMER 5000     // 1s default
 #define ACCHARGE_CONTROL_MAXPOWER 2500  // maximum output power of inverter in W
 #define ACCHARGE_CONTROL_OFFSET \
   2  // offset in % to be subtracted from target power rate to ensure 0 power
@@ -115,7 +115,7 @@
 //    RETRY_TIMER: Determines the time between reconnection [ms]
 //    LED_TIMER: Led blinking rate [ms]
 #define REFRESH_TIMER 1000       // 5s default
-#define WIFI_RETRY_TIMER 60000  // 120s default
+#define WIFI_RETRY_TIMER 120000  // 120s default
 #define LED_TIMER 500            //  0.5s default
 
 #if PINGER_SUPPORTED == 1
