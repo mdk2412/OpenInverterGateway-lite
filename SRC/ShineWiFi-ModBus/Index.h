27879480d27f4bab7cb605eb8b063a09e171ffbe
NeuDLi <23579032+mdk2412@users.noreply.github.com>
2026-06-14 13:58:11 +0200

    Switch to local again

--- SRC/ShineWiFi-ModBus/Index.h
+++ SRC/ShineWiFi-ModBus/Index.h
@@ -7,7 +7,7 @@ const char MAIN_page[] PROGMEM = R"=====(
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Growatt MIN TL-XH</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+  <link rel="stylesheet" href="/pico.min.css">
 </head>
 
 <body>
