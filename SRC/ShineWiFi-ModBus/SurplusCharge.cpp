26779cbbe53ad26ef8bdf1210d5e5b00f55fb6e4
NeuDLi <23579032+mdk2412@users.noreply.github.com>
2026-08-27 21:55:35 +0200

    Formatted and commented out soc

--- SRC/ShineWiFi-ModBus/SurplusCharge.cpp
+++ SRC/ShineWiFi-ModBus/SurplusCharge.cpp
@@ -8,7 +8,7 @@ void surplusCharge() {
   uint64_t power_limit = User.power_limit * 10;
 
   // --- Register EINMAL auslesen ---
-  int32_t soc = Inverter._Protocol.InputRegisters[P3000_BDC_SOC].value;
+  //int32_t soc = Inverter._Protocol.InputRegisters[P3000_BDC_SOC].value;
   int32_t p_chr = Inverter._Protocol.InputRegisters[P3000_BDC_PCHR].value;
   int32_t p_togrid =
       Inverter._Protocol.InputRegisters[P3000_PTOGRID_TOTAL].value;
@@ -18,35 +18,35 @@ void surplusCharge() {
       Inverter._Protocol.HoldingRegisters[P3000_BDC_CHARGE_P_RATE].value;
 
   // --- Delta berechnen ---
-int64_t delta = p_chr + p_togrid - p_touser - power_limit;
-// Log.print("p_togrid: ");
-// Log.println(p_togrid);
-// Log.print("p_touser: ");
-// Log.println(p_touser);
-// Log.print("power_limit: ");
-// Log.println(power_limit);
-// Log.print("p_chr: ");
-// Log.println(p_chr);
-// Log.print("Delta: ");
-// Log.println(delta);
+  int64_t delta = p_chr + p_togrid - p_touser - power_limit;
+  // Log.print("p_togrid: ");
+  // Log.println(p_togrid);
+  // Log.print("p_touser: ");
+  // Log.println(p_touser);
+  // Log.print("power_limit: ");
+  // Log.println(power_limit);
+  // Log.print("p_chr: ");
+  // Log.println(p_chr);
+  // Log.print("Delta: ");
+  // Log.println(delta);
 
-// clamp Delta auf >= 0
-delta = std::max<int64_t>(delta, 0);
+  // clamp Delta auf >= 0
+  delta = std::max<int64_t>(delta, 0);
 
-// Rate berechnen
-int32_t rate = (delta * 10) / max_power;
-// Log.print("rate: ");
-// Log.println(rate);
+  // Rate berechnen
+  int32_t rate = (delta * 10) / max_power;
+  // Log.print("rate: ");
+  // Log.println(rate);
 
-// clamp auf 0–100
-uint16_t targetpowerrate = std::clamp(rate, 0, 100);
+  // clamp auf 0–100
+  uint16_t targetpowerrate = std::clamp(rate, 0, 100);
 
-if (current_rate < targetpowerrate) {
-  char json[64];
-  snprintf(json, sizeof(json), "{\"value\":%d,\"retry\":2}", targetpowerrate);
+  if (current_rate < targetpowerrate) {
+    char json[64];
+    snprintf(json, sizeof(json), "{\"value\":%d,\"retry\":2}", targetpowerrate);
 
-  StaticJsonDocument<256> req, res;
-  Inverter.HandleCommand("bdc/set/chargepowerrate", (const byte*)json,
-                         strlen(json), req, res);
-}
+    StaticJsonDocument<256> req, res;
+    Inverter.HandleCommand("bdc/set/chargepowerrate", (const byte*)json,
+                           strlen(json), req, res);
+  }
 }
