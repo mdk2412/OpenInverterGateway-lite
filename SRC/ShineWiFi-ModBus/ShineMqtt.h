502349774835d41d2ec40608ba8b922e60bb55cd
NeuDLi <23579032+mdk2412@users.noreply.github.com>
2026-08-29 16:19:32 +0200

    Revert "Copilot heap fragmentation optimization"
    This reverts commit e91eedc3c6763dfd8577a684943a96057e6f3c91.

--- SRC/ShineWiFi-ModBus/ShineMqtt.h
+++ SRC/ShineWiFi-ModBus/ShineMqtt.h
@@ -39,6 +39,6 @@ class ShineMqtt {
   Growatt& inverter;
   // Optimierung 4: loop()-Taktung
   uint32_t lastMqttLoop = 0;
-  static void getId(char* buffer, size_t buflen);
+  static String getId();
 };
 #endif
