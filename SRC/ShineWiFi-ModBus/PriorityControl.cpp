d5714096fe85102349a8b7bd71a693764c804ddc
NeuDLi <23579032+mdk2412@users.noreply.github.com>
2026-08-31 17:16:39 +0200

    Adapt priority control logic

--- SRC/ShineWiFi-ModBus/PriorityControl.cpp
+++ SRC/ShineWiFi-ModBus/PriorityControl.cpp
@@ -11,34 +11,34 @@ void priorityControl() {
   uint16_t ptouser_threshold = User.ptouser_thr;
 
   int32_t priority = Inverter._Protocol.InputRegisters[P3000_PRIORITY].value;
-  // int32_t soc = Inverter._Protocol.InputRegisters[P3000_BDC_SOC].value;
+  int32_t soc = Inverter._Protocol.InputRegisters[P3000_BDC_SOC].value;
   int32_t p_togrid =
       Inverter._Protocol.InputRegisters[P3000_PTOGRID_TOTAL].value / 10;
   int32_t p_touser =
       Inverter._Protocol.InputRegisters[P3000_PTOUSER_TOTAL].value / 10;
   int32_t p_disch =
       Inverter._Protocol.InputRegisters[P3000_BDC_PDISCHR].value / 10;
-  int32_t p_ch =
-      Inverter._Protocol.InputRegisters[P3000_BDC_PCHR].value / 10;
+  int32_t p_ch = Inverter._Protocol.InputRegisters[P3000_BDC_PCHR].value / 10;
 
   avg_ptouser += alpha * (p_touser - p_ch - avg_ptouser);
   avg_ptogrid += alpha * (p_togrid - p_disch - avg_ptogrid);
 
   if (priority == 1 && avg_ptouser > ptouser_threshold) {
     StaticJsonDocument<128> req1, res1;
-    Inverter.HandleCommand("priority/set",
-                           (const byte*)"{\"mode\":0,\"retry\":2}",
-                           strlen("{\"mode\":0,\"retry\":2}"), req1, res1);
+    const char* payload1 = "{\"mode\":0,\"retry\":2}";
+    Inverter.HandleCommand("priority/set", (const byte*)payload1,
+                           strlen(payload1), req1, res1);
+
     StaticJsonDocument<128> req2, res2;
-    Inverter.HandleCommand("bdc/set/chargepowerrate",
-                           (const byte*)"{\"value\":100,\"retry\":2}",
-                           strlen("{\"value\":100,\"retry\":2}"), req2, res2);
+    const char* payload2 = "{\"value\":100,\"retry\":2}";
+    Inverter.HandleCommand("bdc/set/chargepowerrate", (const byte*)payload2,
+                           strlen(payload2), req2, res2);
   }
 
-  if (priority == 0 && avg_ptogrid > ptogrid_threshold) {
+  if (priority == 0 && avg_ptogrid > ptogrid_threshold && soc < 95) {
     StaticJsonDocument<128> req1, res1;
-    Inverter.HandleCommand("priority/set",
-                           (const byte*)"{\"mode\":1,\"retry\":2}",
-                           strlen("{\"mode\":1,\"retry\":2}"), req1, res1);
+    const char* payload = "{\"mode\":1,\"retry\":2}";
+    Inverter.HandleCommand("priority/set", (const byte*)payload,
+                           strlen(payload), req1, res1);
   }
 }
