32c8db5501c64e7983b057280053d9e517bbf3b6
NeuDLi <23579032+mdk2412@users.noreply.github.com>
2026-09-08 15:04:22 +0200

    Increase retry number

--- SRC/ShineWiFi-ModBus/SurplusCharge.cpp
+++ SRC/ShineWiFi-ModBus/SurplusCharge.cpp
@@ -36,7 +36,7 @@ void surplusCharge() {
 
     JsonDocument req, res;
     req["value"] = value;
-    req["retry"] = 2;
+    req["retry"] = 3;
 
     Inverter.HandleCommand("bdc/set/chargepowerrate", req, res);
   }
